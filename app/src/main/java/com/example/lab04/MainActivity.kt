8beadc706859871b1026e3ceafe21411e4dbb34a
Nieve2205 <ashleepre2022@gmail.com>
2024-08-31 14:09:44 -0500

    Agregar estilos al Button

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lab04
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,7 +12,9 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -55,18 +58,34 @@ fun MyApp() {
     ) { paddingValues ->
         LazyColumn(
             contentPadding = paddingValues,
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
         ) {
             items(itemsList) { item ->
-                Text(
-                    text = item,
-                    fontSize = 18.sp,
-                    color = Color(0xFF333333),
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
-
-            item {
+                Card(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 4.dp
+                    ),
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color.Gray
+                    )
+                ) {
+                    Text(
+                        text = item,
+                        fontSize = 20.sp,
+                        color = Color(0xFF333333),
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                }
+        }
+        item {
                 Column(modifier = Modifier.padding(16.dp)) {
                     OutlinedTextField(
                         value = text,
